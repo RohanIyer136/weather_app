d1167cb59fca4de4854c5e39ef236d0b3f3ddd71
RohanIyer136 <Rohan.Iyer@dxb.manipal.edu>
2022-07-27 23:56:41 +0400

    first commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'ad1cfe24a8msh6f22e74a938cec1p118bf2jsne5840189ffec',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+export const WEATHER_API_KEY = "bfa97ba04fd3261b7c32eb49415738d4";
